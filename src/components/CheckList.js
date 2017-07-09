3c08f8f1c2ef08a77ea1efbc21567de5d85cc849
Damien <dasseya1@gmail.com>
2017-07-09 12:18:26 -0400

    Finished working on Card, CheckList, and List components

---
+++ src/components/CheckList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+  
+export default class CheckList extends Component {
+    render() {
+      let tasks = this.props.tasks.map((task) => (
+              <li className="checklist__task">
+                  <input type="checkbox" defaultChecked={task.done} />
+                  {task.name}
+                  <a href="#" className="checklist__task--remove"></a>
+              </li>
+            
+          ));
+      return (
+          <div className="checklist">
+              <ul>{tasks}</ul>
+          </div>
+      )
+    }
+  }
