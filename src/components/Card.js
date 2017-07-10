802458d2e608adbdb9f7dae2a96d15ce4b956766
Damien <dasseya1@gmail.com>
2017-07-10 18:13:29 -0400

    Worked on functionalities and made card details togglable

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
   
 export default class Card extends Component {
+    constructor(){
+        super(...arguments);
+        this.state = {
+            showDetails: false
+        };
+    }
+
+    toggleDetails() {
+        this.setState({showDetails: !this.state.showDetails});
+    }
+
     render() {
-      return (
-        <div className="card">
-            <div className="card__title">{this.props.title}</div>
+        let cardDetails;
+        if (this.state.showDetails){
+            cardDetails = (
             <div className="card__details">
                 {this.props.description}
                 <CheckList cardId={this.props.id} tasks={this.props.tasks} />
             </div>
+            );
+        };
+      return (
+        <div className="card">
+            <div className={
+                this.state.showDetails? "card__title card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>   
+                {this.props.title}
+            </div>
+            {cardDetails}
         </div>
       );
     }
