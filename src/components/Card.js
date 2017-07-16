640d48c35a272dc1df572086d2fd83facabd3a0c
Damien <dasseya1@gmail.com>
2017-07-15 23:48:01 -0400

    Added inline style for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -24,12 +24,24 @@ export default class Card extends Component {
             </div>
             );
         };
+
+        let sideColor = {
+            position: 'absolute',
+            zIndex: -1,
+            top: 0,
+            bottom: 0,
+            left: 0,
+            width: 7,
+            backgroundColor: this.props.color
+        };
+        
       return (
         <div className="card">
-            <div className={
-                this.state.showDetails? "card__title card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>   
-                {this.props.title}
-            </div>
+            <div style={sideColor}/>
+                <div className={
+                    this.state.showDetails? "card__title card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>   
+                    {this.props.title}
+                </div>
             {cardDetails}
         </div>
       );
